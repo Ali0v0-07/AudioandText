3d14d9804b6e0cca168e9109ffd676834bc063a8
ALI07 <2542886026@qq.com>
2023-03-16 10:13:56 +0800

    Initial commit

---
+++ AudioandTextConversion/Program.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using WebSocketSharp;
+using ErrorEventArgs = WebSocketSharp.ErrorEventArgs;
+namespace AudioandTextConversion
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            XFapi.Run();
+        }
+    }
+}
